8756896681fd9d5ead2e4e9a884f7c5903aa067a
Logan <juliotrevisan@gmail.com>
2021-06-26 15:43:13 -0300

    substantial improvement to help text

--- gyatso/somecrap.py
+++ gyatso/somecrap.py
@@ -1,21 +1,49 @@
-__all__ = ["get_defaultfont", "render_lines"]
+__all__ = ["get_defaultfont", "render_lines", "TextStyle"]
 
-import pygame, pygame.freetype, gyatso
+import pygame, pygame.freetype, gyatso, os
 
 
 def get_defaultfont():
-    return pygame.freetype.SysFont(gyatso.FONTFAMILY, gyatso.FONTSIZE)
+    # return pygame.freetype.SysFont(gyatso.FONTFAMILY, gyatso.FONTSIZE)
+    filename = os.path.join(os.path.split(__file__)[0], "3270Medium.otf")
+    return pygame.freetype.Font(filename, gyatso.FONTSIZE)
 
 
-def render_lines(surf, lines, color, padding=10, linespacing=10, font=None):
+def render_lines(surf, lines, style=None, padding=10, linespacing=10):
     """Renders text as if the whole screen could be taken with this text"""
-    if font is None: font = get_defaultfont()
-    fontheight = font.render("test8#TAIpy", (0, 0, 0))[1].height
+    if style is None: style = TextStyle()
 
     y = padding
     if not isinstance(lines, list):
         lines = lines.split("\n")
     for line in lines:
-        ot, _ = font.render(line, color)
+        ot, _ = style.font.render(line, style.color)
         surf.blit(ot, (padding, y))
-        y += fontheight+linespacing
+        y += style.fontheight+linespacing
+
+
+class TextStyle:
+    @property
+    def font(self):
+        self.__init_font()
+        return self.__font
+
+    @font.setter
+    def font(self, value):
+        self.__font = value
+
+    @property
+    def fontheight(self):
+        self.__init_font()
+        if self.__fontheight is None:
+            self.__fontheight = self.__font.render("test8#TAIpy", (0, 0, 0))[1].height
+        return self.__fontheight
+
+    def __init__(self, font=None, color=(0, 255, 0)):
+        self.font = font
+        self.__fontheight = None
+        self.color = color
+
+    def __init_font(self):
+        if self.__font is None:
+            self.__font = gyatso.get_defaultfont()
--- gyatso/textcomponent.py
+++ gyatso/textcomponent.py
@@ -1,14 +1,22 @@
 __all__ = ["TextComponent"]
 
 from pygame.locals import *
-import gyatso
+import gyatso, math, pygame
+
 
 class TextComponent(gyatso.Component):
-    def __init__(self, *args, color=(0,255,0), bgcolor=(0,0,0), **kwargs):
+    def __init__(self, *args, color=(0,255,0), bgcolor=(0,0,0), textstyle=None, padding=10, linespacing=10, scrollbarwidth=10, **kwargs):
         super().__init__(*args, **kwargs)
         self.color = color
         self.bgcolor = bgcolor
+        self.textstyle = textstyle if textstyle is not None else gyatso.TextStyle()
+        self.linespacing = linespacing
+        self.padding = padding
+        self.scrollbarwidth = scrollbarwidth
         self.__top = 0  # first line of text to be drawn
+        self.__mode = "from_bottom"
+        self.__flag_first = True
+        self.__lines = []
 
     # ABSTRACT =========================================================================================================
 
@@ -24,20 +32,71 @@ class TextComponent(gyatso.Component):
 
             if key == K_UP:
                 self.__top = self.__top-1 if self.__top > 0 else self.__top  # I know this is ugly but can't be bothered
+                self.__mode = "from_top"
                 ret = self.game.redraw()
 
             elif key == K_DOWN:
                 self.__top = self.__top+1 if self.__top < self.__get_count()-1 else self.__top
+                self.__mode = "from_top"
                 ret = self.game.redraw()
         return ret
 
     def do_draw(self, surf):
-        # game = self.game
-        lines = self._get_lines()
+        def build_ycoords():
+            # n*fontheight+(n-1)*linespacing+padding >= height
+            fh = self.textstyle.fontheight
+            n = math.ceil((height+self.linespacing-self.padding)/(fh+self.linespacing))
+            y1 = height-1-self.padding-fh
+            step = fh+self.linespacing
+            ycoords = [y1-i*step for i in range(n)]
+            return ycoords
+
+        width, height = surf.get_size()
+        ycoords = build_ycoords()
+        numcoords = len(ycoords)
+        lines = self.__lines = self._get_lines()
         surf.fill(self.bgcolor)
-        gyatso.render_lines(surf, lines[self.__top:], self.color)
+
+        numlines = len(lines)
+        if numlines <= numcoords: self.__mode = "from_bottom"  # invalidates otherwise
+        if self.__mode == "from_bottom":
+            iline = numlines-1
+        else:
+            if self.__top+numcoords > numlines:
+                self.__top = numlines-numcoords
+            iline = self.__top+numcoords-1
+        if iline == numlines-1: self.__mode = "from_bottom"
+
+        icoord = 0
+        lastline = iline
+        while icoord < numcoords and iline >= 0:
+            line = lines[iline]
+            y = ycoords[icoord]
+            ot, _ = self.textstyle.font.render(line, self.textstyle.color)
+            surf.blit(ot, (self.padding, y))
+            self.__top = firstline = iline
+            icoord += 1
+            iline -= 1
+
+        # "Scrollbar" (non-responsive though, but TODO one could store the coordinates from this drawing to make it responsive)
+
+        x0 = width-self.scrollbarwidth
+        # x1 = width-1
+        y0 = 0
+        y1 = height-1
+        f_rect = lambda c, w: pygame.draw.rect(surf, color=c, rect=(x0, y0, self.scrollbarwidth, y1-y0+1), width=w)
+        f_rect(self.bgcolor, 0)
+        f_rect(self.color, 1)
+        if numlines > 0:
+            lastline1 = lastline+1
+            a = (y1-y0+1)/numlines
+            b = y0
+            ybox0 = a*firstline+b
+            ybox1 = a*lastline1+b
+            pygame.draw.rect(surf, self.color, (x0, ybox0, self.scrollbarwidth, ybox1-ybox0+1), width=0)
 
     # PRIVATE ==========================================================================================================
+
 
     def __get_count(self):
         # This is a default, slow, implementation
